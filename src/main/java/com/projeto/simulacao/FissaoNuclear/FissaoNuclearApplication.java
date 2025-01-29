0ad8168dbc1e68e09bcedb9f59b891ed9cfe6f76
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-01-29 10:36:32 -0400

    01-Configuration-Files

---
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/FissaoNuclearApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.simulacao.FissaoNuclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FissaoNuclearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FissaoNuclearApplication.class, args);
+	}
+
+}

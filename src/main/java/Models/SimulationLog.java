68d642feb7fe58aa0f4450a42fb89a1c69ce5f36
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-01-30 14:49:29 -0400

    04-Config-Entity-Simulation-Log

--- src/main/java/Models/SimulationLog.java
+++ src/main/java/Models/SimulationLog.java
@@ -1,4 +1,25 @@
 package Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimulationLog{
+
+    @Id
+    private int id;
+
+    private double voidProduction;
+
+    private double waterInject;
+
+    private double energyProduced;
 }

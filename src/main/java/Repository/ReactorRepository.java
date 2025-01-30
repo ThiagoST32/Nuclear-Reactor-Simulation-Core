d67ad6f3a68c6f459bcdee3617290759d5eb0367
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-01-30 16:29:10 -0400

    08-Create-CRUD-For-Reactor-Entity

--- src/main/java/Repository/ReactorRepository.java
+++ src/main/java/Repository/ReactorRepository.java
@@ -1,6 +1,11 @@
 package Repository;
 
+import Models.Reactor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReactorRepository extends JpaRepository {
+import java.util.Optional;
+
+public interface ReactorRepository extends JpaRepository<Reactor, Integer> {
+
+    Optional<Reactor> findById(Integer integer);
 }

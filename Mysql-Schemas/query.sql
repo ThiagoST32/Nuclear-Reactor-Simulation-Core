0d8dc29023f7eda61de50dc72cf194df34b60a34
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-06 17:35:44 -0400

    12-Create-Query.sql-For-Mysql-And-Add-H2-In-Pom

--- Mysql-Schemas/query.sql
+++ Mysql-Schemas/query.sql
@@ -1,12 +1,15 @@
 use nuclearfission;
 
-drop database nuclearfission;
 create database nuclearfission;
+DROP database nuclearfission;
 
 SHOW TABLES;
+
+select * from reactor;
 
 SELECT * FROM chemicalelement;
 SELECT * FROM eventlog;
 SELECT * FROM reactor;
-SELECT * FROM simulation;
-SELECT * FROM reactorsize;
+SELECT * FROM reactor_size;
+SELECT * FROM reactorstate;
+SELECT * FROM simulation_log;

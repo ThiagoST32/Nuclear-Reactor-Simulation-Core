b72513ca3786cd47fd7c143ea7fc50ef4e5a4187
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-06 17:37:47 -0400

    16-Resolve-Repository-SimulationLog-And-Add-New-Schema01.db

--- src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElement.java
+++
@@ -1,6 +0,0 @@
-package com.projeto.simulacao.FissaoNuclear.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChemicalElement extends JpaRepository <ChemicalElement, Integer>{
-}
--- src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/SimulationLog.java
+++
@@ -1,6 +0,0 @@
-package com.projeto.simulacao.FissaoNuclear.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimulationLog extends JpaRepository <SimulationLog, Integer>{
-}

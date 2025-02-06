52b27845a08945673744b66db8be6908ef3fa239
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-06 17:36:08 -0400

    13-Refactor-DTOs

--- src/main/java/com/projeto/simulacao/FissaoNuclear/DTO/ReactorDTO.java
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/DTO/ReactorDTO.java
@@ -2,5 +2,11 @@ package com.projeto.simulacao.FissaoNuclear.DTO;
 
 import com.projeto.simulacao.FissaoNuclear.Models.ReactorSize;
 
-public record ReactorDTO(String reactorType, double maxTemperature, double maxPressure, double fuelCapacity, String moderatorType, ReactorSize reactorSize) {
+public record ReactorDTO(
+        String reactorType,
+        double maxTemperature,
+        double maxPressure,
+        double fuelCapacity,
+        String moderatorType,
+        ReactorSizeDTO reactorSize) {
 }
--- src/main/java/com/projeto/simulacao/FissaoNuclear/DTO/ReactorSizeDTO.java
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/DTO/ReactorSizeDTO.java
@@ -1,4 +1,8 @@
 package com.projeto.simulacao.FissaoNuclear.DTO;
 
-public record ReactorSizeDTO(double diameter, double radius, double wallThickness) {
+public record ReactorSizeDTO(
+        double totalSize,
+        double diameter,
+        double radius,
+        double wallThickness){
 }

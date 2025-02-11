1ebeefa2926f4ebbf22e9ddc5b51e1c0251e0d94
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-11 17:19:08 -0400

    27-Trying-Add-Query-For-Get-ChemicalElement-By-Name

--- src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElementRepository.java
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElementRepository.java
@@ -2,6 +2,11 @@ package com.projeto.simulacao.FissaoNuclear.Repository;
 
 import com.projeto.simulacao.FissaoNuclear.Models.ChemicalElement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChemicalElementRepository extends JpaRepository <ChemicalElement, Integer>{
+
+    /*@Query("SELECT ch FROM chemical_element ch WHERE ch.chemical_name LIKE %:name%")
+    ChemicalElement findChemicalByName(@Param("name") String name);*/
 }

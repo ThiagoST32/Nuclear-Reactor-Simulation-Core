4a7826a22266cec5c46afbb945fbb7eb7815bf5d
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-11 17:18:09 -0400

    26-Add-News-Proprieties

--- src/main/java/com/projeto/simulacao/FissaoNuclear/DTO/ChemicalElementDTO.java
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/DTO/ChemicalElementDTO.java
@@ -2,5 +2,5 @@ package com.projeto.simulacao.FissaoNuclear.DTO;
 
 import com.projeto.simulacao.FissaoNuclear.Models.ChemicalElement;
 
-public record ChemicalElementDTO(Character symbol, int atomicNumber, int massAtomic, int meltingPoint, int boilingPoint){
+public record ChemicalElementDTO(String name, Character symbol, int density , int atomicNumber, int massAtomic, int meltingPoint, int boilingPoint){
 }

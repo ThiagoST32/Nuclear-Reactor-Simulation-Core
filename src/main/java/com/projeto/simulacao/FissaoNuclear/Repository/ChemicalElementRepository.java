b3456ec620a7c1a4565d907f646a0f0fc038dc2e
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-03-10 21:25:37 -0400

    31-Tentativa-De-Resolução

--- src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElementRepository.java
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElementRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ChemicalElementRepository extends JpaRepository <ChemicalElement, Integer>{
+public interface ChemicalElementRepository extends JpaRepository <ChemicalElement, String>{
 
-    /*@Query("SELECT ch FROM chemical_element ch WHERE ch.chemical_name LIKE %:name%")
-    ChemicalElement findChemicalByName(@Param("name") String name);*/
+    @Query(value = "SELECT ch FROM chemical_element WHERE chemical_name LIKE %:name%", nativeQuery = true)
+    ChemicalElement findChemicalByName(@Param("name") String name);
 }

5be91d0a6d89c9a55c70b790e867445d2f026023
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-03-12 19:14:46 -0400

    39-Query-Criada-Para-Busca-De-Elemento-Quimico

--- src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElementRepository.java
+++ src/main/java/com/projeto/simulacao/FissaoNuclear/Repository/ChemicalElementRepository.java
@@ -7,6 +7,13 @@ import org.springframework.data.repository.query.Param;
 
 public interface ChemicalElementRepository extends JpaRepository <ChemicalElement, String>{
 
-    @Query(value = "SELECT ch FROM chemical_element WHERE chemical_name LIKE %:name%", nativeQuery = true)
-    ChemicalElement findChemicalByName(@Param("name") String name);
+    /*@Query(value = "SELECT ch FROM chemical_element WHERE chemical_name LIKE %:name%", nativeQuery = true)
+    ChemicalElement findChemicalByName(@Param("name") String name);*/
+
+    @Query(value = """
+            SELECT CASE WHEN COUNT (chemical_name) > 0 THEN TRUE ELSE FALSE END
+            FROM "CHEMICAL_ELEMENT"
+            WHERE CHEMICAL_NAME = :chemicalName;
+            """, nativeQuery = true)
+    boolean existByName(@Param("chemicalName") String chemicalName);
 }
